e40637676586fdcda8a5b377a6b32d6fb27593ca
Dev-Oero <53870992+HtetAungWin123@users.noreply.github.com>
2024-05-12 10:37:38 +0900

    commit

---
+++ include/footer.php
@@ -0,0 +1,2 @@
+<div class="pull-right hidden-xs"> <b>Version</b> 2.3.0 </div>
+<strong>Copyright &copy; 2022 <a href="https://hawprofile.000webhostapp.com/" target="_blank">Date Event Management</a>.</strong> All rights reserved.
